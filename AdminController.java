292af95afd54db6e91455ca09ddbc4dca41ecf8d
2cpd <147893675+2cpd@users.noreply.github.com>
2023-11-14 17:37:47 -0500

    Add files via upload

---
+++ AdminController.java
@@ -0,0 +1,47 @@
+package application;
+
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class AdminController {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void logout(ActionEvent event) throws IOException {
+		
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Logout?");
+		alert.setHeaderText("Logout of your account?");
+		alert.setContentText("Changes will be saved.");
+		
+		if(alert.showAndWait().get() == ButtonType.OK) {
+			root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	
+	public void help(ActionEvent event) throws IOException {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About This Page");
+		alert.setHeaderText("Administrator Page");
+		alert.setContentText("You can add/delete an user or list all users. To view albums, logout and login with a different account.");
+		alert.showAndWait();
+	}
+}
